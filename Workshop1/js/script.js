fa019fc5ef6892c9ab60412e1125c76c227cd5db
snowball <sharon870130@gmail.com>
2019-03-25 22:25:43 +0800

    煩欸欸欸欸欸為什麼chrome不能正常顯示啊啊啊啊啊啊啊啊啊

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+
 var bookDataFromLocalStorage = [];
 var bookCategoryList = [
     { text: "資料庫", value: "database", src: "image/database.jpg" },
@@ -15,6 +15,7 @@ function loadBookData() {
         bookDataFromLocalStorage = bookData;
         localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
     }
+    
 }
 
 $(function () {
@@ -22,18 +23,81 @@ $(function () {
 });
 
 
-    var datas = new kendo.data.DataSource({
-        data: [{ Id: 1, Name: "one" }, { Id: 2, Name: "two" }],
-        pageSize: 20
-    });
+
+
+$(document).ready(function () {
+
+    $("#add_book_window").hide();
 
     $("#book").kendoGrid({
-        dataSource: datas,
+        dataSource: {
+            data: {
+                "items": bookDataFromLocalStorage
+            },
+            schema: {
+                data: "items"
+            },
+            pageSize: 20
+
+        },
         height: 500,
+
         columns: [
-            { field: "Id", title: "id", width: "50%" },
-            { field: "Name", title: "Name", width: "50%" },
-        ],
-        sortable:true
+            {
+                template: "<button class='delete_book'>刪除</button>",
+                field: "",
+                title: ""
+            },
+            {
+                field: "BookId",
+                title: "書籍編號"
+            },
+            {
+                field: "BookName",
+                title: "書籍名稱",
+            }, {
+                field: "BookCategory",
+                title: "書籍種類",
+            }, {
+                field: "BookAuthor",
+                title: "作者",
+            }, {
+                field: "BookBoughtDate",
+                title: "購買日期",
+            }, {
+                field: "",
+                title: "送達狀態",
+            }, {
+                field: "BookPrice",
+                title: "金額",
+            }, {
+                field: "BookAmount",
+                title: "數量",
+            }, {
+                field: "BookTotal",
+                title: "總計",
+            },
+
+        ]
     });
+
+
+    $(".delete_book").kendoButton();
+
+
+
+});
+
+$("#add_book").click(function () {
+    $("#add_book_window").kendoWindow({
+    width: "600px",
+    title: "新增書籍",
+    visible: false,
+    actions: [
+        "Pin",
+        "Minimize",
+        "Maximize",
+        "Close"
+    ]
+}).data("kendoWindow").center().open();
 });
